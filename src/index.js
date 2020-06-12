40fd3b005e11fc2a3441d0ea60e44418677e64b4
Hwihun Cho <hwihun.cho@navercorp.com>
2020-06-12 16:09:17 +0900

    feat: add test function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { getSemanticReleaseExampleMessage, getAnotherSemanticReleaseExampleMessage } from './SemanticReleaseExampleMessageService';
 
+function test() {
+
+}
+
 export {
+  test,
   getSemanticReleaseExampleMessage,
   getAnotherSemanticReleaseExampleMessage,
 };
